bff02f33ab7d98f9f32721df928db37408174ddb
Tosin Daniel <osdaniel024@gmail.com>
2020-08-20 16:11:24 +1000

    added the adder function

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express')
 var app = express()
 
+var adder = function(num1,num2){
+    var sum = num1 + num2
+    return sum
+}
 
 app.use(express.static(__dirname + '/public'));
+app.get('/test', function(req,res){
+    var username = req.query.username;
+    console.log('I have been hit');
+    res.send('You hit me! '+username);
+})
+
+app.get('/sum', function(req,res){
+    var num1 = parseInt(req.query.num1);
+    var num2 = parseInt(req.query.num2);
+    var sum = adder(num1,num2);
+    res.send('The sum is: '+sum);
+})
+
 //listen to a port
 var port = 3000
 app.listen(port)
